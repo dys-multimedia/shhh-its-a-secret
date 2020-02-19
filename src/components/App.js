2ea3168033192b47a985aa2101185c9eb330ed62
David Soards <davidysoards@gmail.com>
2020-02-19 01:06:00 -0500

    start

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 import Header from './Header';
-import Card from './Card';
-import Info from './Info';
+// import Card from './Card';
+// import Info from './Info';
 
 export default class App extends Component {
   state = {
@@ -48,6 +48,35 @@ export default class App extends Component {
     this.fetchPokemon(this.state.pokemon.next);
   };
 
+  // componentDidMount() {
+  //   this.fetchPokemon(this.state.offset);
+  // }
+
+  // fetchPokemon = async offset => {
+  //   const res = await fetch(
+  //     `https://pokeapi.co/api/v2/pokemon/?limit=${this.state.limit}&offset=${offset}`
+  //   ).catch(error => console.log(error));
+  //   const pokes = await res.json();
+  //   this.setState({ pokemon: pokes });
+  //   console.log(pokes);
+  // };
+
+  // previous = () => {
+  //   if (this.state.offset === 0) return;
+  //   this.setState(prevState => {
+  //     return { offset: prevState.offset - this.state.limit };
+  //   });
+  //   this.fetchPokemon(this.state.offset - this.state.limit);
+  // };
+
+  // next = () => {
+  //   if (this.state.offset > this.state.pokemon.count - this.state.limit) return;
+  //   this.setState(prevState => {
+  //     return { offset: prevState.offset + this.state.limit };
+  //   });
+  //   this.fetchPokemon(this.state.offset + this.state.limit);
+  // };
+
   handleClose = () => {
     this.setState({ isInfoShowing: false });
   };
@@ -72,11 +101,32 @@ export default class App extends Component {
 
         <main id="main-content">
           {isInfoShowing ? (
-            <Info details={details} close={this.handleClose} />
+            <div className="poke-card info">
+              <img src={details.sprites.front_default} alt="" />
+              <h2>{details.name}</h2>
+              <p>Powers:</p>
+              {details.powers.map(power => (
+                <h4 key={power.ability.name}>{power.ability.name}</h4>
+              ))}
+              <button
+                className="btn close"
+                onClick={() => this.setState({ isInfoShowing: false })}
+              >
+                Close
+              </button>
+            </div>
           ) : (
+            // <Info details={details} close={this.handleClose} />
             <ul className="pokemon">
               {pokemon.results.map(poke => (
-                <Card name={poke.name} key={poke.name} select={this.select} />
+                <li
+                  className="poke-card"
+                  key={poke.name}
+                  onClick={() => this.select(poke.name)}
+                >
+                  <h3>{poke.name}</h3>
+                </li>
+                // <Card name={poke.name} key={poke.name} select={this.select} />
               ))}
             </ul>
           )}
